44094a0f991ea780bbf0b1246269f1b290d2a8c9
Daniel Amaya <damayalop@gmail.com>
2024-12-16 23:15:36 -0600

    Add custom colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5E376D",
+        primaryLight: "#D3BBDD",
+        secondary: "#DF2AB7",
+        secondaryLight: "#ECE3F0",
+      },
+    },
   },
   plugins: [],
 };
