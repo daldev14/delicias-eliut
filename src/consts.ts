42ac6d86e05fbc9bf8b5509a2b592ebf3a9ace3b
Daniel Amaya <damayalop@gmail.com>
2024-12-18 21:14:29 -0600

    Add and implement icon component

--- src/consts.ts
+++ src/consts.ts
@@ -1,44 +1,38 @@
-import IconEmail from "@/components/icons/Email.astro";
-import IconFacebook from "@/components/icons/Facebook.astro";
-import IconInstagram from "@/components/icons/Instagram.astro";
-import IconWhatsApp from "@/components/icons/WhatsApp.astro";
-import IconTikTok from "@/components/icons/TikTok.astro";
-
 export const REDES = [
   {
     name: "WhatsApp",
     href: "https://wa.me/+50488934752?text=Buen día Mónica, quisiera saber más información sobre sus servicios.",
-    icon: IconWhatsApp,
+    iconName: "whatsapp",
     isDisable: false,
     title: "Escríbenos por WhatsApp",
   },
   {
     name: "Facebook",
     href: "https://www.facebook.com/profile.php?id=100067779445668&mibextid=ZbWKwL",
-    icon: IconFacebook,
+    iconName: "facebook",
     isDisable: false,
     title: "Visítanos en Facebook",
   },
   {
     name: "Instagram",
     href: "https://www.instagram.com/mocatohn/",
-    icon: IconInstagram,
+    iconName: "instagram",
     isDisable: false,
     title: "Escríbenos por Instagram.",
   },
   {
     name: "TikTok",
     href: "https://www.tiktok.com/@mocatohn",
-    icon: IconTikTok,
+    iconName: "tiktok",
     isDisable: false,
     title: "Visítanos en TikTok",
   },
-  
+
   {
     name: "Email",
     href: "#",
-    icon: IconEmail,
+    iconName: "email",
     isDisable: true,
     title: "Envíanos un correo",
   },
-];
+] as const;
